edbd92f87db1cf0bc9c5a0cb3f86f22950656808
miki0221 <miki.shibutani0221@gmail.com>
2023-01-09 13:47:46 +0900

    変数の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @items = user.items
+    items = user.items
 
     # カテゴリー
-    category_face = Item.where(category_id: 2)
+    category_face = items.where(category_id: 2)
     @post_face = category_face.includes(:user).order("created_at DESC")
 
-    category_eye = Item.where(category_id: 3)
+    category_eye = items.where(category_id: 3)
     @post_eye = category_eye.includes(:user).order("created_at DESC")
 
-    category_lip = Item.where(category_id: 4)
+    category_lip = items.where(category_id: 4)
     @post_lip = category_lip.includes(:user).order("created_at DESC")
 
     # 残数
